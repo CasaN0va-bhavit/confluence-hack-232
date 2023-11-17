5f90b71cefa0d03fab0a4fb1f3402276140b27ec
Shyamak-Seth <sethshyamak@gmail.com>
2023-11-17 18:12:17 +0530

    first commit

---
+++ routers/regRouter.js
@@ -0,0 +1,31 @@
+const router = require('express').Router()
+const User = require('../schemas/userSchema.js')
+const bcrypt = require('bcrypt')
+
+router.get('/', (req, res) => {
+    res.render('register')
+})
+
+router.post('/', async (req, res) => {
+    const {username, fname, lname, password, cnfpassword} = req.body
+    const foundUser = await User.findOne({username})
+    if (!username || !password || !fname || !lname || !cnfpassword) {
+        return res.render('register', {error: 'Please enter all the credentials'})
+    }
+    if (password !== cnfpassword) {
+        return res.render('register', {error: 'The passwords do not match!'})
+    }
+    if (foundUser) return res.render('register', {error: "A user already exists with this username."})
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newUser = new User({
+        username: username,
+        fname: fname,
+        lname: lname,
+        password: hashedPassword
+    })
+    await newUser.save()
+    console.log(`ACCOUNT CREATED`)
+    res.redirect('/')
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+
+const express = require('express'),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    app = express(),
+    flash = require('express-flash'),
+    session = require('cookie-session'),
+    PORT = process.env.PORT || 5000,
+    passportInit = require('./utils/passport-config')
+
+const indexRouter = require('./routers/indexRouter'),
+    regRouter = require('./routers/regRouter')
+    loginRouter = require('./routers/loginRouter')
+    dashboardRouter = require('./routers/dashboardRouter')
+
+mongoose.connect(process.env.MONGO_URI, console.log('MONGODB CONNECTED'))
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json({limit: '1mb'}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+passportInit(passport)
+
+
+app.use('/', indexRouter)
+app.use('/login', loginRouter)
+app.use('/register', regRouter)
+app.use('/dashboard', dashboardRouter)
+
+app.listen(PORT, console.log(`SERVER CONNECTED ON PORT ${PORT}`))
