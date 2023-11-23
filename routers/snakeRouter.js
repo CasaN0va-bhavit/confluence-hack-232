6a8f6fd275899e414c8eb65850822a8931f3eb73
Shyamak-Seth <sethshyamak@gmail.com>
2023-11-23 19:24:27 +0530

    added snake backend and fixed issues

--- routers/snakeRouter.js
+++ routers/snakeRouter.js
@@ -1,7 +1,19 @@
 const router = require('express').Router()
+const User = require('../schemas/userSchema')
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     res.render('snake')
+})
+
+router.post('/', async (req, res) => {
+    const {score} = req.body
+    const foundUser = await User.findOne({username: req.user.username})
+    await User.updateOne({username: req.user.username}, {
+        $set: {
+            coins: (Number(score) * 10) + foundUser.coins
+        }
+    })
+    console.log(await User.findOne({username: req.user.username}))
 })
 
 module.exports = router
